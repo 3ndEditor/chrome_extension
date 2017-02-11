ebebde7e35e5b7e660b5fccef9d78bb52dc594bd
DESKTOP-TAEMIN\taemi <taemin3000@naver.com>
2017-02-11 14:44:19 +0900

    Revert "Revert "tab :hover, dragularModel통한 링크 순서바꾸기""
    This reverts commit ad8c2ac2243c000f18364760b79774fc3b996a54.

--- FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.component.ts
+++ FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Keymap } from '../../../../../shared/keymap/keymap.provider';
+import { animate, Component, OnInit, state, style, transition, trigger } from '@angular/core';
 
 @Component({
     selector:"export",
     templateUrl : 'editorTab.export.component.html',
-    styleUrls:['editorTab.export.component.css'] 
+    styleUrls:['editorTab.export.component.css'],
+    animations:[
+        trigger(
+            'exportActive',[
+                state('deActive', style({ height: '0' })),
+                state('active', style({})),
+                transition('deActive <=> active', animate(300)),
+            ]
+        ),
+    
+    ]
 })
 export class ExportComponent{
-    
+    /**
+    * @file editorTab.export.component.ts
+    * @author KimTaemin 17/02/09
+    * @brief Exporting PDF File
+    * @see Need to translate img, since copyright.
+    * @todo intersect with server
+    */
+
+    private isExportActive: boolean;
+    constructor(private keymap: Keymap) {
+        this.isExportActive = false;   
+    }
+
+    openExport() {
+        this.isExportActive = this.isExportActive ? false : true;
+        console.log("openExport클릭됨")
+    }
 }
--- FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.module.ts
+++ FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.module.ts
@@ -1,9 +1,10 @@
+import { SharedModule } from '../../../../../shared/shared.module';
 import { ExportComponent } from './editorTab.export.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SharedModule],
     exports: [ExportComponent],
     declarations: [ExportComponent],
     providers: []
