dd2cf16bf3abb9111129a8faa3d2bdf36d53b58a
cicada0014 <cicada0014@gmail.com>
2017-02-25 20:02:32 +0900

    iframe 사용 테스트…

--- FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.component.ts
+++ FrontAngular/src/app/section/outline/tab/editorTab/export/editorTab.export.component.ts
@@ -1,25 +1,25 @@
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subject } from 'rxjs/Rx';
 import { handleError } from '../../../../../async-handling.promise';
 import { Http, Jsonp, ResponseContentType, URLSearchParams } from '@angular/http';
 import { Keymap } from '../../../../../shared/keymap/keymap.provider';
-import { animate, Component, OnInit, state, style, transition, trigger } from '@angular/core';
+import { animate, Component, ElementRef, OnInit, Renderer, state, style, transition, trigger } from '@angular/core';
 
 @Component({
-    selector:"export",
-    templateUrl : 'editorTab.export.component.html',
-    styleUrls:['editorTab.export.component.css'],
-    animations:[
+    selector: "export",
+    templateUrl: 'editorTab.export.component.html',
+    styleUrls: ['editorTab.export.component.css'],
+    animations: [
         trigger(
-            'exportActive',[
+            'exportActive', [
                 state('deActive', style({ height: '0' })),
                 state('active', style({})),
                 transition('deActive <=> active', animate(300)),
             ]
         ),
-    
+
     ]
 })
-export class ExportComponent{
+export class ExportComponent {
     /**
     * @file editorTab.export.component.ts
     * @author KimTaemin 17/02/09
@@ -29,8 +29,8 @@ export class ExportComponent{
     */
 
     private isExportActive: boolean;
-    constructor(private keymap: Keymap, private jsonp : Jsonp ,private http:Http  ) {
-        this.isExportActive = false;   
+    constructor(private keymap: Keymap, private jsonp: Jsonp, private http: Http, private el: ElementRef, private renderer: Renderer) {
+        this.isExportActive = false;
     }
 
     openExport() {
@@ -38,20 +38,48 @@ export class ExportComponent{
         console.log("openExport클릭됨")
     }
 
-    connectServerTest() {
-        let testUrl="http://localhost:8080/hello?callback=JSONP_CALLBACK";
+    connectPreTest() {
+        let testUrl = "http://www.google.com";
         let params = new URLSearchParams();
-        // params.set('format','json');
-        // params.set('callback','JSONP_CALLBACK');
-        
+        params.set('format', 'json');
+        params.set('callback', 'JSONP_CALLBACK');
+
         console.log("서버호출");
         // jsonp은 json을 콜백으로 한번 감싼 형태이다. http://dev.epiloum.net/1311 참고 사이트.
         //
-        return this.jsonp.get(testUrl).toPromise().then(response=>console.log(response.json()));
-        // return this.http.get(testUrl).toPromise().then(response=>{console.log(JSON.parse(response.text()).name +"ttt111t///"+response.text())});
-        
-        
+
+        return this.jsonp.get(testUrl).toPromise().then((response)=>
+            this.renderer.selectRootElement(this.el.nativeElement).insertAdjacentHTML('beforeend', response.json().name)
+            );
+        // return this.http.get(testUrl)
     }
 
 
-}
+        operator: Observable<string>;
+        testStream = new Subject<string>();
+        connectServerTest() {
+            // let testUrl="http://www.naver.com";
+            // let params = new URLSearchParams();
+            // params.set('format','json');
+            // params.set('callback','JSONP_CALLBACK');
+
+            this.connectPreTest()
+            // .subscribe((info)=>{
+            //     console.log(info)
+            };
+            // // jsonp은 json을 콜백으로 한번 감싼 형태이다. http://dev.epiloum.net/1311 참고 사이트.
+            // //
+
+            // // return this.jsonp.get(testUrl).toPromise().then((response)=>
+            // //     this.renderer.selectRootElement(this.el.nativeElement).insertAdjacentHTML('beforeend', response.json().name)
+            // //     );
+            // return this.jsonp.get(testUrl,params).map(response=>{console.log("("+response.json() +")")}).;
+            // // return this.http.get(testUrl).toPromise().then(response=>{console.log(JSON.parse(response.text()).name +"ttt111t///"+response.text())});
+
+
+            //  this.operator
+            // JSON.parse(this.operator.to)   
+        }
+
+
+    
