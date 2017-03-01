c97922ba816c23a85aacd7c452370a61aebe740a
cicada0014 <cicada0014@gmail.com>
2017-03-02 04:56:43 +0900

    editor table 적용 / 버튼 전면 수정 / fontawesome css 적

--- FrontAngular/src/app/app.module.ts
+++ FrontAngular/src/app/app.module.ts
@@ -5,23 +5,19 @@ import { SectionModule } from './section/section.module';
 import { InvalidUrlPageComponent } from './invalid-url-page.component';
 import { LoginUserService } from './user/user';
 import { UserResolveService } from './user/user-resolve.service';
-
-
 import { HeaderModule } from './header/header.module';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,InvalidUrlPageComponent
+    AppComponent, InvalidUrlPageComponent
   ],
   imports: [
     BrowserModule,
@@ -31,14 +27,16 @@ import { AppComponent } from './app.component';
     HeaderModule,
     SectionModule,
     JsonpModule,
+    Angular2FontAwesomeModule
+    
     // 앵귤라에 최적화된 마테리얼 디자인을 쓸 수 있으나 아직 베타버전이고 성능이 구짐. 나중에 좋아지면 적용하자.
     // 적용하면 최소 1초는 더 연장됨.... 쓰지 말자.
     // MaterialModule.forRoot()
-    
-    
+
+
 
   ],
-  providers: [AuthGuard,CanDeactivateGuard ,UserResolveService,LoginUserService,NavBarService],
+  providers: [AuthGuard, CanDeactivateGuard, UserResolveService, LoginUserService, NavBarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FrontAngular/src/app/section/outline/editor/editor.component.ts
+++ FrontAngular/src/app/section/outline/editor/editor.component.ts
@@ -1,10 +1,10 @@
 import { EditorModule } from './editor.module';
 import { Component, ElementRef, OnInit, Renderer, Input } from '@angular/core';
-import * as UsingEditor from 'medium-editor';
-import * as MediumEditorTable1 from 'medium-editor-tables';
+// import * as MediumEditorTable1 from 'medium-editor-tables';
+// import * as UsingEditor from 'medium-editor';
 //head에 선언된 medium_editor 자바스크립트를 역참조한다.
-
- 
+ declare var MediumEditor:any
+ declare var MediumEditorTable:any;
 
 
 @Component({
@@ -24,7 +24,7 @@ export class EditorComponent implements OnInit {
     @Input() routeData: string;
     editorOptions: MediumEditor.CoreOptions = {
         activeButtonClass: 'medium-editor-button-active',
-        buttonLabels: false,
+        buttonLabels: 'fontawesome',
         // 에디터 사용유무
         disableEditing: false,
         // 툴바 옵션
@@ -55,7 +55,7 @@ export class EditorComponent implements OnInit {
                 // 'h4',
                 // 'h5',
                 // 'h6',
-                'tabel',
+                'table',
                 'quote', // 인용구
                 'strikethrough', //취소선
                 'subscript',  // 아래쪽에 조그맣게
@@ -86,12 +86,12 @@ export class EditorComponent implements OnInit {
             sticky: false,
             updateOnEmptySelection: true
         },
-        // extensions: {
-        //     'table': new MediumEditorTable({
-        //         rows: 40,
-        //         columns: 40
-        //     })
-        // },
+        extensions: {
+            'table': new MediumEditorTable({
+                rows: 40,
+                columns: 40
+            })
+        },
         placeholder: {
             /* This example includes the default options for placeholder,
                if nothing is passed this is what it used */
@@ -144,8 +144,8 @@ export class EditorComponent implements OnInit {
     constructor(public el: ElementRef, public renderer: Renderer) {
         //  에디터 생성!!
         // @types 에 타입을 지정해준다. 다만 사용법이 다 달라서 해당 내부를 뜯어봐야 어떻게 가져다 쓸지 보인다.
-        new UsingEditor(renderer.selectRootElement(el.nativeElement), this.editorOptions);
-
+        
+        new MediumEditor(this.renderer.selectRootElement(this.el.nativeElement), this.editorOptions);
 
 
         // 에디터에 대한 커스텀 이벤트 리스너 추가 
@@ -154,7 +154,9 @@ export class EditorComponent implements OnInit {
             var data = $event.dataTransfer.getData
         })
     }
-
+    ngAfterViewInit(){
+        
+    }
     ngOnInit() { }
     ngOnChanges() {
         // 이 메소드를 쓰면 무엇이든지 붙일 수가 있다.
