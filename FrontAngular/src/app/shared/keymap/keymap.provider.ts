c2406c7770815cef514a4f8e1bcafa79774bf474
cicada0014 <cicada0014@gmail.com>
2017-03-02 20:09:06 +0900

    setting section 제작시작 / 키맵 api provider 형태에서 service 형태로 변경(동적 선택을 위해) / 앱 라우팅 일부 변경

--- FrontAngular/src/app/shared/keymap/keymap.provider.ts
+++ FrontAngular/src/app/shared/keymap/keymap.provider.ts
@@ -1,33 +1,26 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Keymap {
-
-    
-    help_key: number;
-    wide_key: number;
-    create_frame_key:number;
-    header_fixed_key:number;
-
-    
-
-}
-
-let customKeymap = {
-    help_key: 112, //f1
-    wide_key: 87, // w
-    create_frame_key: 83, //s
-    header_fixed_key: 69  //e
-    
 
 
-}
 
-export let KeymapProvider = {
-    provide: Keymap,
-    useValue: customKeymap
 
-}
+
+
+
+
+
+
+
+// let KeymapFactory = (userKeymap:Keymap)=>{
+
+//     return userKeymap;
+// }
+
+// export let KeymapsProvier = {
+//     provide: Keymap,
+//     useFactory: KeymapFactory,
+//     deps:[
+//         Keymap
+//     ]
+// }
 
 // 값제공자는 상위 모듈에서 한번만 주입하면 그 하위 모듈들도 전부 이용가능하다 
 // 이용하는 방법은 그 타입을 가지고 있는 변수를 컴포넌트가 가지고 있기만 하면 자동주입이다. 
