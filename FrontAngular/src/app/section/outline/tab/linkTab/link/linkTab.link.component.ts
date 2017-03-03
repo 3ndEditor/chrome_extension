1247f6c8972d9d82a9d1153ebc4f10284f6d9a37
DESKTOP-TAEMIN\taemi <taemin3000@naver.com>
2017-03-03 09:23:11 +0900

    iframe에 링크값 전달 준비

--- FrontAngular/src/app/section/outline/tab/linkTab/link/linkTab.link.component.ts
+++ FrontAngular/src/app/section/outline/tab/linkTab/link/linkTab.link.component.ts
@@ -2,7 +2,7 @@ import { Links } from './links';
 import { FormsModule } from '@angular/forms';
 import { Link } from './link';
 import { MockService } from '../mock/mock.service';
-import { Component, Provider, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Provider } from '@angular/core';
 import { DragulaService, dragula } from 'ng2-dragula/ng2-dragula';
 
 
@@ -20,6 +20,8 @@ export class LinkComponent {
     * @see 
     * @todo intersect with server, make method
     */
+
+    @Output() outputURL = new EventEmitter<string>();
 
     // 서버 Json을 받는 리스트 맴버
     listLinks : Links[] = [];
@@ -42,12 +44,15 @@ export class LinkComponent {
         console.log("비고정링크"+this.listLinkUnfixed[0].fixed[1]+this.listLinkUnfixed[0].url+"/"+this.listLinkFixed[1].fixed[1]+this.listLinkFixed[1].url);
     }
 
-    
     getTitle(url:string){
         
     }
 
-    openURL(url:string){
-        console.log(url+"을 로드합니다.")
+    // openURL(url:string){
+    //     console.log(url+"을 로드합니다.")
+    // }
+
+    sendURL(url:string){
+        this.outputURL.emit(url);
     }
 }
